a8ad0f207eec2c9bd5b63c10905bb48a43d762c8
Gastery <tipzov@yandex.ru>
2022-12-26 13:45:32 +0300

    add GetArr2

--- task1/Program.cs
+++ task1/Program.cs
@@ -6,4 +6,15 @@
 string[] arr1 = new string[5] {"Vas", "fF", ":-)", "12345", "pol"};
 string[] arr2 = new string[arr1.Length];
 
-
+void GetArr2(string[] arr1, string[] arr2)
+{
+    int count = 0;
+    for (int i = 0; i < arr1.Length; i++)
+    {
+    if(arr1[i].Length <= 3)
+        {
+        arr2[count] = arr1[i];
+        count++;
+        }
+    }
+}
