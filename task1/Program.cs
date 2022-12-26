79ce324a181dbe9d4a84dd1849ea6b1fd6bab45a
Gastery <tipzov@yandex.ru>
2022-12-26 13:50:44 +0300

    add PrintArray

--- task1/Program.cs
+++ task1/Program.cs
@@ -18,3 +18,16 @@ void GetArr2(string[] arr1, string[] arr2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+GetArr2(arr1, arr2);
+PrintArray(arr2);
+
+
